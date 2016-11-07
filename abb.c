78b7b872eb043ac8df441bd4f9938ca26f0600ba
IT <it@Is-iMac.local>
2016-11-07 13:20:17 -0300

    abb_iter_in_crear bug arreglado

--- abb.c
+++ abb.c
@@ -172,10 +172,7 @@ abb_iter_t *abb_iter_in_crear(const abb_t *arbol) {
 	}
 	iter->pila = pila;
 	nodo_abb_t* act = arbol->raiz;
-	if (!act) {
-		return iter;
-	}
-	while(act->izq) {
+	while(act && act->izq) {
 		pila_apilar(iter->pila, act->clave);
 		act = act->izq;
 	}
